0ec6b0890c599b9b445cf5c19414af0ee36d178a
Mario Hamming <perezfernan159@gmail.com>
2025-07-27 17:39:23 -0500

    Primer commit Literalura

---
+++ src/main/java/com/aluracursos/Challengue_Literalura/ChallengueLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Challengue_Literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengueLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengueLiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/Challengue_Literalura/ChallengueLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Challengue_Literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengueLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
